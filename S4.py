289d5d023b7310b42634ffb18372db570ec3977a
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2023-10-01 21:20:44 +0330

    Session4 before solve

---
+++ S4.py
@@ -0,0 +1,42 @@
+from tkinter import *
+class Login():
+    def __init__(self, root, bg=None, fg=None, font=None):
+        self.r = root
+        self.root = LabelFrame(self.r, text="Login", bg=bg, fg=fg, font=font)
+        self.label_username = Label(self.root, text="Username: ", bg=bg, fg=fg, font=font)
+        self.label_password = Label(self.root, text="Password: ", bg=bg, fg=fg, font=font)
+        self.entry_username = Entry(self.root, bg=bg, fg=fg, font=font)
+        self.entry_password = Entry(self.root, bg=bg, fg=fg, font=font, show="•")
+        self.show_password = BooleanVar(self.root)
+        self.checkbutton = Checkbutton(self.root, bg=bg, fg=fg, variable=self.show_password, font=font, command=self.check)
+        self.btn_login = Button(self.root, text='Enter',bg=bg, fg=fg, font=font)
+        self.label_username.grid(row=1, column=1)
+        self.label_password.grid(row=2, column=1)
+        self.entry_username.grid(row=1, column=2)
+        self.entry_password.grid(row=2, column=2)
+        self.checkbutton.grid(row=2, column=3)
+        self.btn_login.grid(row=3, column=1)
+    def check(self):
+        if self.show_password.get():
+            self.entry_password.config(show="")
+        else:
+            self.entry_password.config(show="•")
+    def grid(self, row=None, column=None):
+        self.root.grid(row=row, column=column)
+
+class Login2(Login):
+    def __init__(self, root, bg=None, fg=None, font=None):
+        super().__init__(root, bg, fg, font)
+        self.btn_reset = Button(self.root, text='Reset',bg=bg, fg=fg, font=font, command=self.reset)
+        self.btn_reset.grid(row=3, column=2)
+    def reset(self):
+        self.entry_username.delete(0, END)
+        self.entry_password.delete(0, END)
+        self.entry_username.focus_set()
+
+root=Tk()
+f1 = Login(root, bg='#333333', fg='orange', font=('consolas', 24))
+f1.grid()
+f2 = Login2(root, bg='#333333', fg='orange', font=('consolas', 24))
+f2.grid()
+root.mainloop()
---
+++ tamrins/3.py
@@ -0,0 +1,74 @@
+from tkinter import *
+from tkinter.ttk import Combobox
+from tkinter import filedialog
+import json
+class Personal_Form():
+    def __init__(self, root, title = 'Patient\'s Personal Information', l1='Name: ', l2='Surname: ', l3='Father Name:',
+                 l4='National Code: ', l5='Age: ', l6='Blood Type', padx=10, pady=10, bg=None, fg=None, font=None):
+        self.root = root
+        self.frame = LabelFrame(self.root, text=title, bg=bg, fg=fg, font=font, labelanchor='n', padx=padx, pady=pady)
+        self.label1 = Label(self.frame, text=l1, bg=bg, fg=fg, font=font, padx=padx, pady=pady)
+        self.label2 = Label(self.frame, text=l2, bg=bg, fg=fg, font=font, padx=padx, pady=pady)
+        self.label3 = Label(self.frame, text=l3, bg=bg, fg=fg, font=font, padx=padx, pady=pady)
+        self.label4 = Label(self.frame, text=l4, bg=bg, fg=fg, font=font, padx=padx, pady=pady)
+        self.label5 = Label(self.frame, text=l5, bg=bg, fg=fg, font=font, padx=padx, pady=pady)
+        self.label6 = Label(self.frame, text=l6, bg=bg, fg=fg, font=font, padx=padx, pady=pady)
+        self.entry1 = Entry(self.frame, bg=bg, fg=fg, font=font)
+        self.entry2 = Entry(self.frame, bg=bg, fg=fg, font=font)
+        self.entry3 = Entry(self.frame, bg=bg, fg=fg, font=font)
+        self.entry4 = Entry(self.frame, bg=bg, fg=fg, font=font)
+        self.age = Spinbox(self.frame, from_=1, to=99, state='readonly', readonlybackground=bg, width=20, wrap=True, justify='center', fg=fg, font=font)
+        self.blood_type = Combobox(self.frame, values=['A', 'B', 'O', 'AB'], justify='center', state='readonly', width=20, font=font)
+        self.btnreset = Button(self.frame, text='Reset', bg=bg, fg=fg, font=font, padx=padx, pady=pady, command=self.reset)
+        self.btnsave = Button(self.frame, text='Save', bg=bg, fg=fg, font=font, padx=padx, pady=pady, command=self.save)
+        self.label1.grid(row=1, column=1, sticky='news')
+        self.label2.grid(row=2, column=1, sticky='news')
+        self.label3.grid(row=3, column=1, sticky='news')
+        self.label4.grid(row=4, column=1, sticky='news')
+        self.label5.grid(row=5, column=1, sticky='news')
+        self.label6.grid(row=6, column=1, sticky='news')
+        self.entry1.grid(row=1, column=2, sticky='news')
+        self.entry2.grid(row=2, column=2, sticky='news')
+        self.entry3.grid(row=3, column=2, sticky='news')
+        self.entry4.grid(row=4, column=2, sticky='news')
+        self.age.grid(row=5, column=2, sticky='news')
+        self.blood_type.grid(row=6, column=2, sticky='news')
+        self.btnreset.grid(row=7, column=1, sticky='news')
+        self.btnsave.grid(row=7, column=2, sticky='news')
+        self.reset()
+    def save(self):
+        f_name = filedialog.asksaveasfilename()
+        f_name+='.json'
+        f = open(f_name, 'w')
+        info = {
+            'Name': self.entry1.get(),
+            'Surname': self.entry2.get(),
+            'Father Name': self.entry3.get(),
+            'National Code': self.entry4.get(),
+            'Age': self.age.get(),
+            'Blood Type': self.blood_type.get(),
+        }
+        json.dump(info, f, indent=4, ensure_ascii=False)
+        f.close()
+    def reset(self):
+        self.entry1.delete(0, END)
+        self.entry2.delete(0, END)
+        self.entry3.delete(0, END)
+        self.entry4.delete(0, END)
+        self.age.config(state='normal')
+        self.age.delete(0, END)
+        self.age.insert(0, 12)
+        self.age.config(state='readonly')
+        self.blood_type.config(state='normal')
+        self.blood_type.delete(0, END)
+        self.blood_type.insert(0, 'A')
+        self.blood_type.config(state='readonly')
+        self.entry1.focus_set()
+        
+    def grid(self, row=None, column=None):
+        self.frame.grid(row=row, column=column)
+
+root = Tk()
+form1 = Personal_Form(root, bg='sky blue', fg='purple', font=('Serif', 20))
+form1.grid()
+mainloop()
